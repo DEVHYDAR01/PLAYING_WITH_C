9805b16b3e729282cc8edf64e25a0b71a7943459
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-30 00:31:32 +0100

    This commit will push my patterns

---
+++ patterns.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int i;
+	int j;
+
+	for (i = 0; i < 5; i++)
+	{
+	
+		for (j = 0; j < 5; j++)
+		{
+		
+			printf("#");
+		}
+		printf("\n");
+	}
+	return (0);
+}
