4000b5905a84844b0702ac127aabeb941daad057
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-10-11 15:54:29 +0100

    This commit will some modification

--- moremalloc.c
+++ moremalloc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(void)
@@ -7,22 +8,20 @@ int main(void)
 	int i, n;
 	char *ptr;
 	char str [] = "hydar";
-	char name;
+	int length = strlen(str);
 
-	printf("enter how many\n");
-	scanf("%d", &n);
+//	printf("enter how many\n");
+//	scanf("%d", &n);
 	ptr = (char *) malloc(n * sizeof(char));
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		name = str[i];
-		//printf("%c", name);
-	}
+//	for (i = 0; str[i] != '\0'; i++)
+//	{
+//		name = str[i];
+//		printf("%c", name);
+//	}
 	
-	for (i = 0; ptr[i] < n; i++)
+	for (i = 0; i <= length; i++)
 	{
-		ptr[i];
-		name = ptr[i];
-		printf("%c", name);
+		ptr[i] = str[i];
 	}
-	return (0);
-}
+	printf("copied string %s\n", ptr);
+	return (0);}
