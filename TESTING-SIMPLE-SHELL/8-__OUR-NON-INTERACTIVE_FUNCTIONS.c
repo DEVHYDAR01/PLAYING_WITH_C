67ac7fceea88e186ebd224535b97771fd8995742
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-10-16 21:05:34 +0100

    This commit will push my command line interpreter in testing

---
+++ TESTING-SIMPLE-SHELL/8-__OUR-NON-INTERACTIVE_FUNCTIONS.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * non_interactive - handles non_interactive mode
+ *
+ * Return: void
+ */
+
+void __our_non_interactive(void)
+{
+	char **current_command = NULL;
+	int i, type_command = 0;
+	size_t n = 0;
+
+	if (!(isatty(STDIN_FILENO)))
+	{
+		while (getline(&line, &n, stdin) != -1)
+		{
+			__our_remove_newline(line);
+			__our_remove_newline(line);
+			commands = __our_tokenizeinput(line, ";");
+			for (i = 0; commands[i] != NULL; i++)
+			{
+				current_command = __our_tokenizeinput(commands[i], " ");
+				if (current_command[0] == NULL)
+				{
+					free(current_command);
+					break;
+				}
+				type_command = parse_command(current_command[0]);
+				__our_initializer(current_command, type_command);
+				free(current_command);
+			}
+			free(commands);
+		}
+		free(line);
+		exit(status);
+	}
+}
